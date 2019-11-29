66ba9d2e50d21978260e29c06ed137e42329a689
Thomas Baker <thomas.baker@autogravity.com>
2019-11-28 16:51:30 -0800

    Tutorial 5 complete

--- src/main.js
+++ src/main.js
@@ -1,9 +1,34 @@
-var roleHarvester = require('role.harvester.js');
+const roleHarvester = require('role.harvester');
+const roleUpgrader = require('role.upgrader');
+const roleBuilder = require('role.builder');
 
 module.exports.loop = function () {
 
-    for(var name in Game.creeps) {
-        var creep = Game.creeps[name];
-        roleHarvester.run(creep);
+    let tower = Game.getObjectById('TOWER_ID');
+    if (tower) {
+        let closestDamagedStructure = tower.pos.findClosestByRange(FIND_STRUCTURES, {
+            filter: (structure) => structure.hits < structure.hitsMax
+        });
+        if (closestDamagedStructure) {
+            tower.repair(closestDamagedStructure);
+        }
+
+        let closestHostile = tower.pos.findClosestByRange(FIND_HOSTILE_CREEPS);
+        if (closestHostile) {
+            tower.attack(closestHostile);
+        }
     }
-}
+
+    for (let name in Game.creeps) {
+        let creep = Game.creeps[name];
+        if (creep.memory.role === 'harvester') {
+            roleHarvester.run(creep);
+        }
+        if (creep.memory.role === 'upgrader') {
+            roleUpgrader.run(creep);
+        }
+        if (creep.memory.role === 'builder') {
+            roleBuilder.run(creep);
+        }
+    }
+};
